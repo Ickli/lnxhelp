7dcf1dc441f79538163815a735fd9bc380092cf2
Ickli <danic.ponomarew@gmail.com>
2023-08-22 18:17:22 +0500

    tweak msys script

--- cmake/cmake_createlib.sh
+++ cmake/cmake_createlib.sh
@@ -1,3 +1,15 @@
+# DESCRIPTION:
+# Creates directory with passed name
+# Creates CmakeLists.txt in the dir
+# Writes to CmakeLists.txt following text:
+# ---
+# add_library(${dirname})
+# target_include_directories(${dirname} ./)
+# ---
+# ARGUMENTS:
+# dirname
+
+
 dirname=$1
 dirpath=$(realpath ${dirname})
 cmakelists="${dirpath}/CMakeLists.txt"
